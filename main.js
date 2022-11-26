343aa6a1fc9760817ec4c665eb219e2415ba29c7
Ricardo Rosendo <ricardorosendo98@hotmail.com>
2022-11-26 15:58:31 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,40 @@
+const submit = document.querySelector('button');
+
+function validate(a, b) {
+    if (!a || !b) {
+        alert ("Campo A e B precisa conter algum valor.")
+        return {
+            validar: false,
+            menssage: 'Preencha os números para A e B'
+        }
+    } else if  (b >= a) {
+        return {
+            validar: true,
+            menssage: 'Parabéns este valor e igual ou mair que o Campo A'
+        }
+    }
+    alert ("Digite um valor maior no campo B por favor!")
+    return {
+        validar: false,
+        menssage: 'O Campo B precisa ser igual ou maior que o Campo A'
+    }
+}
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const feedback = document.querySelector('.feedback');
+
+    const a = document.querySelector('#a').value;
+    const b = document.querySelector('#b').value;
+
+    const { validar, menssage } = validate(a, b);
+
+    if (validar) {
+        feedback.classList.add('valid');
+        feedback.classList.remove('invalid');
+    } else {
+        feedback.classList.remove('valid');
+        feedback.classList.add('invalid');
+    }
+
+    feedback.innerHTML = menssage;
+})
